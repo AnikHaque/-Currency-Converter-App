2515839b7e6783d7da4a11b79951bb4f5a2c8a2b
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-28 05:54:58 +0600

    createed CurrencyConverter class

---
+++ anik_currency_converter.py
@@ -0,0 +1,11 @@
+# CurrencyConverter ক্লাস তৈরি করা হয়েছে যেটি মুদ্রা রূপান্তরের কাজ করে
+
+class CurrencyConverter:
+    # ক্লাস অ্যাট্রিবিউট: mock exchange rates
+    exchange_rates = {
+        'USD': 1.0,
+        'EUR': 0.85,
+        'GBP': 0.75,
+        'INR': 83.0,
+        'BDT': 110.0,
+    }
