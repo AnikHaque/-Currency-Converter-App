253ba38de0c245c977d50849a2aa26b82830ddf9
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-28 05:57:03 +0600

    set instance attribute

--- anik_currency_converter.py
+++ anik_currency_converter.py
@@ -9,3 +9,11 @@ class CurrencyConverter:
         'INR': 83.0,
         'BDT': 110.0,
     }
+  def __init__(self, amount, from_currency, to_currency, logger):
+     
+        # ইনস্ট্যান্স অ্যাট্রিবিউট সেট করা হচ্ছে
+    
+        self.amount = amount
+        self.from_currency = from_currency.upper()
+        self.to_currency = to_currency.upper()
+        self.logger = logger  # Association - Logger ক্লাসের অবজেক্ট ইনপুট হিসেবে নিচ্ছে
